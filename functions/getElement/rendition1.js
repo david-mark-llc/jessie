d490be72157807e2f23d8466886629bf4cd1fe69
adambsilver <adambsilver@gmail.com>
2011-12-14 19:37:17 +0000

    standardising the JS, single quotes, improved naming conventions, style of conditionals etc

--- functions/getElement/rendition1.js
+++ functions/getElement/rendition1.js
@@ -1,4 +1,14 @@
-if (isHostMethod(document, "getElementById")) { 
+/*
+See: https://groups.google.com/forum/#!starred/comp.lang.javascript/fVp-DWAIGnc
+
+That's the most basic rendition: single document and no allowance for 
+crewy markup like this:- 
+
+<input name="test"> 
+<input id="test"> 
+*/
+
+if (isHostMethod(document, 'getElementById')) { 
   var getElement = function(id) { 
     return document.getElementById(id); 
   }; 
