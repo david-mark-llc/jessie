6ab6a9bfc130f771a9a5d55ff9aa0b17415b2249
Adam Silver <adambsilver@gmail.com>
2011-12-15 13:58:10 +0000

    added getEventTarget renditions

---
+++ functions/getEventTarget/rendition1.js
@@ -0,0 +1,11 @@
+if(html && isHostMethod(html, 'addEventListener')) {
+	var getEventTarget = function(e) { 
+	  var target = e.target; 
+	  // Check if not an element (e.g. a text node) 
+	  if (1 != target.nodeType) { 
+	    // Set reference to parent node (which must be an element) 
+	    target = target.parentNode; 
+	  } 
+	  return target; 
+	};
+}
---
+++ functions/getEventTarget/rendition2.js
@@ -0,0 +1,20 @@
+/*
+See: https://groups.google.com/forum/#!starred/comp.lang.javascript/uUsSVH7Vcvg
+
+If you will be using a forked rendition to support IE 8-
+*/
+
+var getEventTarget = function(e) { 
+  var target = e.target; 
+  if (target) { 
+    // Check if not an element (e.g. a text node) 
+    if (1 != target.nodeType) { 
+      // Set reference to parent node (which must be an element) 
+      target = target.parentNode; 
+    } 
+  } 
+  else { 
+    target = e.srcElement; 
+  } 
+  return target; 
+} 
