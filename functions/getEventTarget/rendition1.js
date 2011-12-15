7346dea4ba03dac3946ca6e201f2f45a4d5b3537
Adam Silver <adambsilver@gmail.com>
2011-12-15 16:46:36 +0000

    moved variable declaration out of conditional

--- functions/getEventTarget/rendition1.js
+++ functions/getEventTarget/rendition1.js
@@ -1,5 +1,8 @@
+
+var getEventTarget;
+
 if(html && isHostMethod(html, 'addEventListener')) {
-	var getEventTarget = function(e) { 
+	getEventTarget = function(e) { 
 	  var target = e.target; 
 	  // Check if not an element (e.g. a text node) 
 	  if (1 != target.nodeType) { 
