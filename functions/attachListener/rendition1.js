3a47ed7c2981c9ef47559561d3903a794d5bc306
Adam Silver <adambsilver@gmail.com>
2011-12-15 13:23:08 +0000

    added second rendition for IE8- for attachListener

--- functions/attachListener/rendition1.js
+++ functions/attachListener/rendition1.js
@@ -1,5 +1,11 @@
+/*
+
+Degrades in IE 8- 
+
+*/
+
 if(html && isHostMethod(html, 'addEventListener')) {
 	var attachListener = function(el, eventType, fn) { 
 		el.addEventListener(eventType, fn, false); 
-	}; 
+	};
 }
---
+++ functions/attachListener/rendition2.js
@@ -0,0 +1,11 @@
+/*
+
+Works in IE 8-
+
+*/
+
+if(html && isHostMethod(html, 'attachEvent')) {
+	var attachListener = function(el, eventType, fn) { 
+		el.attachEvent("on"+eventType, fn); 
+	};
+}
