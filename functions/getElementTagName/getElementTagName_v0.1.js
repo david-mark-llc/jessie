c393a61a12484c8d11ce6cfcfefce6b340febb91
Paul Connolly <paulconnolly@gmail.com>
2011-12-16 08:55:29 +0000

    Added getElementTagName and getElementParent to help with getAncestorByTagName

---
+++ functions/getElementTagName/getElementTagName_v0.1.js
@@ -0,0 +1,5 @@
+var getElementTagName;
+getElementTagName = function (el) {
+	var tagName = (el.tagName || el.nodeName).toLowerCase();
+	return tagName.indexOf('html:') > -1 ? tagName.substring(5) : tagName;
+}
