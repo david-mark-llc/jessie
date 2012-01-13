569a33fdd26d57ca97bd1a7a7499caa0902645e2
Adam Silver <adambsilver@gmail.com>
2012-01-13 15:31:12 +0000

    Added hasClass rendition

---
+++ functions/hasClass/rendition1.js
@@ -0,0 +1,6 @@
+var hasClass;
+if(html && "string" == typeof html.className) {
+	hasClass = function(el, className) {
+		return (new RegExp('(^|\\s)' + className + '(\\s|$)')).test(el.className);
+	};
+}
