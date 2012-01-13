d2442e54e14c69604962eb17c804639ae4fc4cc9
Adam Silver <adambsilver@gmail.com>
2012-01-13 14:58:12 +0000

    Added rendition for removeClass

---
+++ functions/removeClass/rendition1.js
@@ -0,0 +1,16 @@
+var removeClass;
+if(html && "string" == typeof html.className) {
+	removeClass = function(el, className) {
+		var re, m;
+		if (el.className) {
+		  if (el.className == className) {
+			el.className = '';
+		  }
+		  else {		
+			re = new RegExp('(^|\\s)' + className + '(\\s|$)');
+			m = el.className.match(re);
+			if (m && m.length == 3) { el.className = el.className.replace(re, (m[1] && m[2])?' ':''); }
+		  }
+		}
+	};
+}
