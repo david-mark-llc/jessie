6c6355c84d9ba606bd55630059b137100c1d4575
Paul Connolly <paulconnolly@gmail.com>
2012-01-17 10:39:10 +0000

    used the new convention for typeof comparison

--- functions/findProprietaryStyle/rendition1.js
+++ functions/findProprietaryStyle/rendition1.js
@@ -1,11 +1,11 @@
 var findProprietaryStyle;
 if(html){
 	findProprietaryStyle = function (style) {		
-		if (typeof html.style[style] != 'string') {
+		if ('string' != typeof html.style[style]) {
 			style = style.charAt(0).toUpperCase() + style.substring(1);
 			var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'ms'];
 			for (var i = prefixes.length; i--; ) {
-				if (typeof html.style[prefixes[i] + style] != 'undefined') {
+				if ('undefined' != typeof html.style[prefixes[i] + style]) {
 					return prefixes[i] + style;
 				}
 			}
