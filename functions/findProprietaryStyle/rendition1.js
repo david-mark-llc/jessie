d964942d9386f505b60bc9ebc07aac1dacde7480
Paul Connolly <paulconnolly@gmail.com>
2012-01-17 09:59:08 +0000

    Fixed typo with Ms prefix

--- functions/findProprietaryStyle/rendition1.js
+++ functions/findProprietaryStyle/rendition1.js
@@ -3,7 +3,7 @@ if(html){
 	findProprietaryStyle = function (style) {		
 		if (typeof html.style[style] != 'string') {
 			style = style.charAt(0).toUpperCase() + style.substring(1);
-			var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'Ms'];
+			var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'ms'];
 			for (var i = prefixes.length; i--; ) {
 				if (typeof html.style[prefixes[i] + style] != 'undefined') {
 					return prefixes[i] + style;
