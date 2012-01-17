d6e78f6bf3292422ea7e29b2e9ac7cd9003ed2eb
Paul Connolly <paulconnolly@gmail.com>
2012-01-17 10:37:36 +0000

    updated to use canCall

--- functions/forEach/rendition2.js
+++ functions/forEach/rendition2.js
@@ -1,6 +1,6 @@
 //For browsers without native
 var forEach;
-if (Function.prototype.call) {
+if (canCall) {
     forEach = function(elements, callback, thisObject) {
 		for (var i = 0, l = elements.length; i < l; i++) {
 			callback.call(thisObject, elements[i], i, elements);
--- functions/forEach/rendition3.js
+++ functions/forEach/rendition3.js
@@ -6,7 +6,7 @@ if (Array.prototype.forEach) {
       elements.forEach(callback, thisObject);
 	};
 }
-else if (Function.prototype.call) {
+else if (canCall) {
     forEach = function(elements, callback, thisObject) {
 		for (var i = 0, l = elements.length; i < l; i++) {
 			callback.call(thisObject, elements[i], i, elements);
