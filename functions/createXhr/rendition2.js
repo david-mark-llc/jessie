bef72b910e7af3a64cee85498cd5484ac28ab885
Adam Silver <adambsilver@gmail.com>
2012-03-21 11:27:30 +0000

    added other renditions taken from My Library for createXhr

---
+++ functions/createXhr/rendition2.js
@@ -0,0 +1,14 @@
+// IE 6 users that have updated their msxml dll files. 
+
+var createXhr;
+
+if(isHostMethod(global, 'ActiveXObject')) {
+	try {
+		if(new global.ActiveXObject('Msxml2.XMLHTTP.6.0')) {
+			createXhr = function() {
+				return new global.ActiveXObject('Msxml2.XMLHTTP.6.0');
+			};
+		}
+	}
+	catch(e) {};
+};
---
+++ functions/createXhr/rendition3.js
@@ -0,0 +1,14 @@
+// for fully patched Win2k SP4 and up 
+
+var createXhr;
+
+if(isHostMethod(global, 'ActiveXObject')) {
+	try {
+		if(new global.ActiveXObject('Msxml2.XMLHTTP.3.0')) {
+			createXhr = function() {
+				return new global.ActiveXObject('Msxml2.XMLHTTP.3.0');
+			};
+		}
+	}
+	catch(e) {};
+};
