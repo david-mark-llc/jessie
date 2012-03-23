c9b3da3784812a201390e85ca987eb22113da1d4
Adam Silver <adambsilver@gmail.com>
2012-03-23 15:39:33 +0000

    query now uses document as opposed to globalDocument which gets nulled at end of IIFE and added rendition for isInQuery

--- functions/query/rendition2.js
+++ functions/query/rendition2.js
@@ -2,6 +2,6 @@ var query;
 
 if(globalDocument && isHostMethod(globalDocument, 'querySelectorAll') && toArray) {
 	query = function(selector, doc) {
-		return toArray((doc || globalDocument).querySelectorAll(selector));
+		return toArray((doc || document).querySelectorAll(selector));
 	};
 };
