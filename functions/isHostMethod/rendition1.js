a7ff4577948a7a17afd83280612f64e51eff047f
Adam Silver <adambsilver@gmail.com>
2012-03-28 08:09:13 +0100

    added rendition for undelegateQueryListener

--- functions/isHostMethod/rendition1.js
+++ functions/isHostMethod/rendition1.js
@@ -1,5 +1,5 @@
 function isHostMethod(object, method) {
-	var reFeaturedMethod = new RegExp('^(function|object)$', 'i');
 	var type = typeof object[method];
-	return !!((reFeaturedMethod.test(type) && object[method]) || type == 'unknown');
+	// Must check object[method] as typeof null is "object"
+	return !!((type == "function" || type == "object" && object[method]) || type == 'unknown');
 };
