86d06c95cf689b873711bd50933b08ec72f4d866
Adam Silver <adambsilver@gmail.com>
2012-04-27 08:03:35 +0100

    edited delegateBoundListener and added delegateBoundQueryListener

--- functions/delegateBoundListener/rendition1.js
+++ functions/delegateBoundListener/rendition1.js
@@ -1,10 +1,11 @@
 var delegateBoundListener;
 
-if(delegateListener && bind) {
+if(attachListener && bind) {
 	delegateBoundListener = function(el, eventType, fn, fnDelegate, thisObject) {
 		var listener = bind(function(e) {
-			if(fnDelegate(getEventTarget(e))) {
-				fn.call(thisObject, e);
+			var sourceNode = fnDelegate(getEventTarget(e));
+			if(sourceNode) {
+				fn.call(thisObject, e, sourceNode);
 			}
 		}, thisObject);
 		
