a14c66b9a0ca492443eb1fdd02a19b5ebb03fb5c
adambsilver <adambsilver@gmail.com>
2012-05-14 19:23:06 +0100

    fixes for every/some

--- functions/every/rendition1.js
+++ functions/every/rendition1.js
@@ -1,7 +1,6 @@
 /*global every:true,forEach */
 if(Array.prototype.every) {
-	every = function(obj, iterator, context) {
-		if(obj == null) return true;
-		return obj.every(iterator, context);
+	every = function(arr, iterator, context) {
+		return arr.every(iterator, context);
 	};
 }
--- functions/some/rendition1.js
+++ functions/some/rendition1.js
@@ -1,8 +1,7 @@
 /*global some:true */
 
 if(Array.prototype.some) {
-	some = function(obj, iterator, context) {
-		if(obj == null) return true;
-		return obj.some(iterator, context);
+	some = function(arr, iterator, context) {
+		return arr.some(iterator, context);
 	};
 }
