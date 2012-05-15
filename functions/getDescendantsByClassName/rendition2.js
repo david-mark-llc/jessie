538892eebe782f3029a1e49d594882990fe40fa8
Adam Silver <adambsilver@gmail.com>
2012-05-15 14:33:19 +0100

    second rendition for getDescendantsByClassName

---
+++ functions/getDescendantsByClassName/rendition2.js
@@ -0,0 +1,19 @@
+/*global getDescendantsByClassName:true,getDescendantsByTagName,hasClass */
+
+var getDescendantsByClassName;
+
+if(getDescendantsByTagName && hasClass) {
+		getDescendantsByClassName = function(el, className) {
+			var elements = getDescendantsByTagName(document, '*'),
+				element,
+				i,
+				elementsWithClassName = [];
+			for(i = 0; i < elements.length; i++) {
+				element = elements[i];
+				if(hasClass(element, className)) {
+					elementsWithClassName.push(element);
+				}
+			}
+			return elementsWithClassName;
+		};
+}
