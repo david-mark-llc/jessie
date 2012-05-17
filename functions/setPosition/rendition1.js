5726684494de8aea3fa62675ca2df1d0792a5aba
Paul Connolly <paulconnolly@gmail.com>
2012-05-17 10:34:48 +0100

    Added renditions for setPosition and setSize

---
+++ functions/setPosition/rendition1.js
@@ -0,0 +1,10 @@
+var positionElement;
+if(html){
+  positionElement = (function(el) {  
+    var px = (typeof html.style.top == 'number') ? 0 : 'px';
+    return function(el, x, y) {
+      if (x !== null) { el.style.left = x + px; }
+      if (y !== null) { el.style.top = y + px; }
+    };
+  })();
+}
---
+++ functions/setSize/rendition1.js
@@ -0,0 +1,10 @@
+var sizeElement;
+if(html){
+  sizeElement = (function() {
+    var px = (typeof html.style.top == 'number') ? 0 : 'px';
+    return function(el, h, w) {
+      if (h !== null && h >= 0) { el.style.height = h + px; }
+      if (w !== null && w >= 0) { el.style.width = w + px; }
+    };
+  })();
+}
