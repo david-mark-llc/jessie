1db14f4a0a00cc24c0fd811bc39c782071a22657
Paul Connolly <paulconnolly@gmail.com>
2012-05-17 10:34:04 +0100

    Fixed bug with html element being used when out of scope, signature now requires and element

--- functions/findProprietaryStyle/rendition1.js
+++ functions/findProprietaryStyle/rendition1.js
@@ -1,19 +1,18 @@
 /*global findProprietaryStyle:true,html */
 
 var findProprietaryStyle;
-
-if(html){
-	findProprietaryStyle = function (style) {
-		if ('string' != typeof html.style[style]) {
-			style = style.charAt(0).toUpperCase() + style.substring(1);
-			var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'ms'];
-			for (var i = prefixes.length; i--; ) {
-				if ('undefined' != typeof html.style[prefixes[i] + style]) {
-					return prefixes[i] + style;
-				}
+	
+findProprietaryStyle = function (style, el) {
+	if ('string' != typeof el.style[style]) {
+		style = style.charAt(0).toUpperCase() + style.substring(1);
+		var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'ms'];
+		for (var i = prefixes.length; i--; ) {
+			if ('undefined' != typeof el.style[prefixes[i] + style]) {
+				return prefixes[i] + style;
 			}
-			return null;
 		}
-		return style;
-	};
-}
+		return null;
+	}
+	return style;
+};
+
