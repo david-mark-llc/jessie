ec6d11c0d2e6046321c8a7d2d8085cb155f1227a
Paul Connolly <paulconnolly@gmail.com>
2012-05-21 07:18:55 +0100

    Tried to improve file for merging

--- functions/findProprietaryStyle/rendition1.js
+++ functions/findProprietaryStyle/rendition1.js
@@ -1,19 +1,19 @@
 /*global html,isHostObjectProperty */
 
 var findProprietaryStyle;
-if(html && isHostObjectProperty(html, "style")){
-  findProprietaryStyle = function (style, el){
-    if ('string' != typeof el.style[style]){
-        style = style.charAt(0).toUpperCase() + style.substring(1);
-        var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'ms'];
-        for (var i = prefixes.length; i--; ) {
-        if ('undefined' != typeof el.style[prefixes[i] + style]){
-          return prefixes[i] + style;
-        }
-      }
-      return null;
-	}
-    return style;
-  };
-}
 
+if(html && isHostObjectProperty(html, "style")){
+	findProprietaryStyle = function (style, el) {
+		if ('string' != typeof el.style[style]) {
+			style = style.charAt(0).toUpperCase() + style.substring(1);
+			var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'Ms'];
+			for (var i = prefixes.length; i--; ) {
+				if ('undefined' != typeof el.style[prefixes[i] + style]) {
+					return prefixes[i] + style;
+				}
+			}
+			return null;
+		}
+		return style;
+	};
+}
