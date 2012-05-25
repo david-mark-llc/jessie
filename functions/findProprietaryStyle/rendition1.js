01f663d3987fd759a4af8a6a22ddcb7e8dd99441
Paul Connolly <paulconnolly@gmail.com>
2012-05-25 11:31:19 +0100

    Rolling back rendition

--- functions/findProprietaryStyle/rendition1.js
+++ functions/findProprietaryStyle/rendition1.js
@@ -1,14 +1,14 @@
-/*global html,isHostObjectProperty */
+/*global html */
 
 var findProprietaryStyle;
 
-if(html && isHostObjectProperty(html, "style")){
-	findProprietaryStyle = function (style, el) {
-		if ('string' != typeof el.style[style]) {
+if(html){
+	findProprietaryStyle = function (style) {
+		if ('string' != typeof html.style[style]) {
 			style = style.charAt(0).toUpperCase() + style.substring(1);
-			var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'Ms'];
+			var prefixes = ['Moz', 'O', 'Khtml', 'Webkit', 'ms'];
 			for (var i = prefixes.length; i--; ) {
-				if ('undefined' != typeof el.style[prefixes[i] + style]) {
+				if ('undefined' != typeof html.style[prefixes[i] + style]) {
 					return prefixes[i] + style;
 				}
 			}
