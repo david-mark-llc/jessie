b9baa0e3be5617de1c0eaa129398bccee1642d10
Adam Silver <adambsilver@gmail.com>
2012-08-01 15:55:42 +0100

    rendition3 both version added for bind

---
+++ functions/bind/rendition3.js
@@ -0,0 +1,33 @@
+/*global canCall */
+
+/*
+Description:
+Relies on `Function.prototype.bind` and `Function.prototype.apply` and `Array.prototype.slice`
+*/
+
+/*
+Degrades:
+TBC
+*/
+
+var bind;
+
+if(canCall && Function.prototype.bind){
+    bind = function(fn, thisObject) {
+        return fn.bind.apply(fn, Array.prototype.slice.call(arguments, 1));
+    };
+}
+else if(canCall && Array.prototype.slice) {
+	bind = function(fn, context) {
+		var prependArgs = Array.prototype.slice.call(arguments, 2);
+
+		if (prependArgs.length) {
+			return function() {
+				fn.apply(context, Array.prototype.concat.apply(prependArgs, arguments));
+			};
+		}
+		return function() {
+			fn.apply(context, arguments);
+		};
+	};
+}
