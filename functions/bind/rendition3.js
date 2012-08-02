e414da669789a8ad099a231aa0e5aebf7d8e2d16
Ian Crowther <maxlee7_jedi@hotmail.com>
2012-08-02 17:55:32 +0100

    Added degrades info

--- functions/bind/rendition3.js
+++ functions/bind/rendition3.js
@@ -5,6 +5,11 @@ Description:
 Both W3C and MS implementation therefore providing the greatest browser support
 */
 
+/*
+Degrades:
+IE8, Chrome 6, Firefox 3.6, Safari 5.1, Opera 11.5
+*/
+
 var bind;
 
 if(canCall && Function.prototype.bind){
