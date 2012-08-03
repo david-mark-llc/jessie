9681f9a2103a76685638b9d72ae8debf2cd38b5d
Ian Crowther <maxlee7_jedi@hotmail.com>
2012-08-03 16:13:44 +0100

    rolled back mixin

--- functions/mixin/rendition1.js
+++ functions/mixin/rendition1.js
@@ -13,10 +13,8 @@ In browsers without `Object.prototype.hasOwnProperty`
 if(Object.prototype.hasOwnProperty) {
 	mixin = function(target, source) {
 		for(var property in source) {
-			if(!target.hasOwnProperty(property)) {
+			if(source.hasOwnProperty(property)) {
 				target[property] = source[property];
-			} else {
-				mixin(target[property], source[property]);
 			}
 		}
 	};
