acbb4ba01a6a667376c48367088e3b4ef80ff844
David Mark <dmark@cinsoft.net>
2012-08-06 18:04:22 -0300

    Update functions/mixin/rendition1.js
    Added TODO for other rendition(s)

--- functions/mixin/rendition1.js
+++ functions/mixin/rendition1.js
@@ -9,6 +9,9 @@ Relies on `jessie.isOwnProperty`
 Degrades:
 */
 
+// TODO: Test the old iteration bug with shadowed built-in properties (e.g. toString)
+//       Need another iteration that handles that bug
+
 if(jessie.isOwnProperty) {
 	mixin = function(target, source) {
 		for(var property in source) {
