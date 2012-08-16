e6fd3ed1f7d72c7376d61d841c6d16b868bf0d2f
Ian Crowther <maxlee7_jedi@hotmail.com>
2012-08-16 15:29:08 +0100

    added 3rd rendition

---
+++ functions/addClass/rendition3.js
@@ -0,0 +1,30 @@
+/*global html,isHostObjectProperty,isHostMethod */
+
+/*
+Description:
+Relies on the 'el.classList.add' and falls back to 'el.className'
+*/
+
+/*
+Degrades:
+IE5, Chrome 3, Firefox 2, Safari 3, Opera 10
+*/
+
+var addClass;
+
+if (html && isHostObjectProperty(html, "classList") && isHostMethod(html.classList, "add") ) {
+	addClass = function(el, className) {
+		return el.classList.add(className);
+	};
+} else if (html && "string" === typeof html.className ) {
+    addClass = function(el, className) {
+      var re;
+      if (!el.className) {
+        el.className = className;
+      }
+      else {
+        re = new RegExp('(^|\\s)' + className + '(\\s|$)');
+        if (!re.test(el.className)) { el.className += ' ' + className; }
+      }
+    };
+}
