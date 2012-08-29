87c247c3c52f3365eba620303c5210f86070d988
Adam Silver <adambsilver@gmail.com>
2012-08-29 14:27:34 +0100

    ajax related functions now take an optional thisObject

--- functions/jsonGet/rendition1.js
+++ functions/jsonGet/rendition1.js
@@ -1,4 +1,4 @@
-/*global ajaxGet,parseJson*/
+/*global ajaxGet,parseJson,bind*/
 
 /*
 Description:
@@ -11,13 +11,16 @@ Degrades:
 
 var jsonGet;
 
-if(ajaxGet && parseJson) {
+if(ajaxGet && parseJson && bind) {
 	jsonGet = function(url, options) {
 		options = options || {};
 		var	wrappedSuccessFn,
 			originalFunction = options.success;
+
 		if(originalFunction) {
 			wrappedSuccessFn = function(response, xhr) {
+				options.thisObject = options.thisObject || xhr;
+				originalFunction = bind(originalFunction, options.thisObject);
 				var json = parseJson(response);
 				originalFunction(json, xhr);
 			};
