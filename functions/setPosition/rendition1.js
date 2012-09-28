1c0306eabe2b1d339629d9747b9ea4edd55056fa
Adam Silver <adambsilver@gmail.com>
2012-09-28 08:30:08 +0100

    getViewportScrollPosition and other bits

--- functions/setPosition/rendition1.js
+++ functions/setPosition/rendition1.js
@@ -2,7 +2,7 @@
 
 var setPosition;
 if(html && isHostObjectProperty(html, "style")){
-  setPosition = (function(el) {  
+  setPosition = (function(el) {
     var px = (typeof html.style.top == 'number') ? 0 : 'px';
     return function(el, x, y) {
       if (x !== null) { el.style.left = x + px; }
