62d03a8effccb0b785388c3420aa6eafc30f238f
feedMyPixel <ben@feedmypixel.com>
2012-11-16 11:41:28 +0000

    added descendant walk if targets tagName does not initially match the specified delegate tagName

--- functions/delegateTagNameListener/rendition1.js
+++ functions/delegateTagNameListener/rendition1.js
@@ -16,10 +16,22 @@ if(delegateListener && getElementTagName) {
 	delegateTagNameListener = function(el, eventType, tagName, fn) {
 		
 		var fnDelegate = function(el, target) {
-			var sourceNode;
+			var sourceNode,
+                descendant;
+
 			if(getElementTagName(target) === tagName) {
 				sourceNode = target;
-			}
+			} else {
+                descendant = target.parentNode;
+
+                while (descendant !== el) {
+                    if (getElementTagName(descendant) === tagName) {
+                        sourceNode = descendant;
+                        break;
+                    }
+                    descendant = descendant.parentNode;
+                }
+            }
 			return sourceNode;
 		};
 
