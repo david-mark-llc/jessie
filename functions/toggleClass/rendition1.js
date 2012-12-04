707d37ae45e1d4eb7a9d97784004ea83a9e1f895
Ben Chidgey <ben@feedmypixel.com>
2012-12-04 16:17:14 +0000

    added rendition1 of toggleClass, a simple wrapper for addClass and removeClass

---
+++ functions/toggleClass/rendition1.js
@@ -0,0 +1,20 @@
+/*global hasClass,addClass,removeClass */
+
+/*
+ Description:
+ Relies on the `jessie.hasClass` && `jessie.addClass` && `jessie.removeClass`
+ */
+
+/*
+ Author:
+ Ben Chidgey
+ */
+
+var toggleClass;
+
+if (hasClass && addClass && removeClass) {
+    toggleClass = function(el, className) {
+        var toggle = hasClass(el, className) ? 'remove' : 'add';
+        jessie[toggle + 'Class'](el, className);
+    };
+}
