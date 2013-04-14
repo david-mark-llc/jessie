16ef87c48df16a17bdf42d84ac5fdf18c548e856
cinsoft <dmark@cinsoft.net>
2013-04-13 22:35:50 -0400

    Add getYearWeekWeekday calendar function

---
+++ functions/getYearWeekWeekday/rendition1.js
@@ -0,0 +1,23 @@
+/* */
+
+var getYearWeekWeekday;
+
+/*
+ * getYearWeekWeekday
+ * 
+ * Converts (Year, Month, Day) to (Year, Week, Day of Week)
+ * 
+ */
+    
+getYearWeekWeekday = function(y, m, d) { // ISO 8601 WkNo. m=1..12
+    
+    // Based on work by Dr JR Stockton (http://www.merlyn.demon.co.uk/)
+    
+    var ms1d = 864e5, ms7d = 7 * ms1d;
+    var DC3 = Date.UTC( y, m - 1, d + 3 ) / ms1d;   // an Absolute Day Number
+    var DoW = 1 + ( DC3 + 7777777 ) % 7;
+    var AWN = Math.floor( DC3 / 7 );                // an Absolute Week Number
+    var Wyr = new Date( AWN * ms7d ).getUTCFullYear();
+
+    return [Wyr, AWN - Math.floor(Date.UTC(Wyr, 0, 7) / ms7d) + 1, DoW]; // Array
+}
