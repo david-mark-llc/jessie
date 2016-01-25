e092d63183a8d204fd3e3176e78b0b4b4d50c2a7
David Mark <dmark@cinsoft.net>
2016-01-25 17:09:05 -0500

    White space issues

--- src/functions/bind/rendition2.js
+++ src/functions/bind/rendition2.js
@@ -12,12 +12,12 @@ IE5, IE4, IE3
 
 var bind;
 
-if(Function.prototype.bind){
+if (Function.prototype.bind) {
 	bind = function(fn, thisObject) {
 		return fn.bind.apply(fn, Array.prototype.slice.call(arguments, 1));
 	};
 }
-else if(canCall && Array.prototype.slice) {
+else if (canCall && Array.prototype.slice) {
 	bind = function(fn, context) {
 		var prependArgs = Array.prototype.slice.call(arguments, 2);
 
