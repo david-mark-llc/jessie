1f2b21c7157d43ec1c5026d349c0048b216e4f61
David Mark <dmark@cinsoft.net>
2016-01-25 17:15:50 -0500

    Rendition, not iteration

--- src/functions/mixin/rendition1.js
+++ src/functions/mixin/rendition1.js
@@ -12,7 +12,7 @@ Degrades:
 */
 
 // TODO: Test the old iteration bug with shadowed built-in properties (e.g. toString)
-//       Need another iteration that handles that bug
+//       Need another rendition that handles that bug
 
 if(isOwnProperty) {
 	mixin = function(target, source) {
