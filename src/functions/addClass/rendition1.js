3c7b3adf9d9762d472afa19086412fe9271a3fe1
David Mark <david.mark@cinsoft.biz>
2016-11-23 12:38:33 -0500

    Update rendition1.js

--- src/functions/addClass/rendition1.js
+++ src/functions/addClass/rendition1.js
@@ -19,6 +19,6 @@ var addClass;
 
 if (html && isHostObjectProperty(html, "classList") && isHostMethod(html.classList, "add") ) {
 	addClass = function(el, className) {
-		return el.classList.add(className);
+		el.classList.add(className);
 	};
 }
