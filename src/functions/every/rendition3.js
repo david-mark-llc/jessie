ce005ae891ab7fc5dea4b8a1737a37e94d780e74
David Mark <david.mark@cinsoft.biz>
2016-11-25 19:33:45 -0500

    Fix white space
    No tabs, please!

--- src/functions/every/rendition3.js
+++ src/functions/every/rendition3.js
@@ -7,19 +7,21 @@ Relies on `Array.prototype.every` or `Function.prototype.call` and `jessie.forEa
 
 var every;
 
-if(Array.prototype.every) {
-	every = function(arr, iterator, context) {
-		return arr.every(iterator, context);
-	};
-} else if(canCall && forEach) {
-	every = function(arr, iterator, context) {
-		var result = true;
-		// TODO: this is slightly inefficient, as it doesn't break out of the
-		// loop when the first `falsy` value is encountered
-		forEach(arr, function(value, index, list) {
-			result = result && iterator.call(context, value, index, list);
-		});
+if (Array.prototype.every) {
+    every = function(arr, iterator, context) {
+        return arr.every(iterator, context);
+    };
+} else if (canCall && forEach) {
+    every = function(arr, iterator, context) {
+        var result = true;
+	    
+        // TODO: this is slightly inefficient, as it doesn't break out of the
+        // loop when the first `falsy` value is encountered
+	    
+        forEach(arr, function(value, index, list) {
+            result = result && iterator.call(context, value, index, list);
+        });
 
-		return result;
-	};
+        return result;
+    };
 }
