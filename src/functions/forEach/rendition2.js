42d3b0989e00a3320f758e58b5f4cc036340fc86
David Mark <david.mark@cinsoft.biz>
2016-12-07 18:57:56 -0500

    Fix whitespace
    No tabs please

--- src/functions/forEach/rendition2.js
+++ src/functions/forEach/rendition2.js
@@ -8,9 +8,9 @@ Relies on `Function.prototype.call` for browsers without native forEach
 var forEach;
 
 if (canCall) {
-	forEach = function(elements, callback, thisObject) {
-		for (var i = 0, l = elements.length; i < l; i++) {
-			callback.call(thisObject, elements[i], i, elements);
-		}
-	};
+        forEach = function(elements, callback, thisObject) {
+                for (var i = 0, l = elements.length; i < l; i++) {
+		        callback.call(thisObject, elements[i], i, elements);
+                }
+        };
 }
