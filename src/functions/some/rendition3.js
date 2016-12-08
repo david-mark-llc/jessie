084dbd2bfb868844d9cfa9da8768ed8fc085d304
David Mark <david.mark@cinsoft.biz>
2016-12-07 19:06:55 -0500

    Inconsistency with - this - object
    Default is *not* the Array object

--- src/functions/some/rendition3.js
+++ src/functions/some/rendition3.js
@@ -18,8 +18,6 @@ if (Array.prototype.some) {
         some = function(arr, iterator, context) {
                 var i = 0, l = arr.length;
 
-                context = context || arr;
-
                 for( ; i < l; i++ ) {
                         if (iterator.call(context, arr[ i ], i, arr)) {
                                 return true;
