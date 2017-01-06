637f2a52dd05903ec4a517738a5ec501b3bce9c4
David Mark <david.mark@cinsoft.biz>
2017-01-06 16:14:51 -0500

    Clean up whitespace

--- src/functions/toArray/rendition3.js
+++ src/functions/toArray/rendition3.js
@@ -13,32 +13,23 @@ Graham Veal
 var toArray;
 
 if (canCall && Array.prototype.slice) {
+        try {
+                Array.prototype.slice.call(arguments, 0);
 
-	try {
-
-		Array.prototype.slice.call(arguments, 0);
-
-		toArray = function(a) {
-			return Array.prototype.slice.call(a, 0);
-		};
-
-	} catch(e) {}
-
+                toArray = function(a) {
+                        return Array.prototype.slice.call(a, 0);
+                };
+        } catch(e) {}
 }
 
 if (!toArray) {
-
-	toArray = function(a) {
-
-		var result = [],
-			i = 0,
-			l = a.length;
-
-		for ( ; i < l; i++) {
-
-			result[i] = a[i];
-		}
-
-		return result;
-	};
+        toArray = function(a) {
+                var result = [], i = 0, l = a.length;
+		
+                for ( ; i < l; i++) {
+                        result[i] = a[i];
+                }
+                
+                return result;
+        };
 }
